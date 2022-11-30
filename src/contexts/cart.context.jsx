7aefed8ba320235fba016fc0188526703f9337b6
lvdlrs <lenmuel.dlrs@gmail.com>
2022-12-01 07:50:59 +0800

    Adding checkout page, increment and decrement functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 
-const addCartItem = (cartItems, productToAdd) =>{
+const addCartItem = (cartItems, productToAdd ) =>{
     // find if cartitems contains productToAdd
     const existingCartItem = cartItems.find( (cartItem ) => cartItem.id === productToAdd.id );
     // if found increment quantity
@@ -14,11 +14,28 @@ const addCartItem = (cartItems, productToAdd) =>{
     return [...cartItems, { ...productToAdd, quantity: 1 }]
 };
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    // find cart item to remove
+    const existingCartItem = cartItems.find( (cartItem ) => cartItem.id === cartItemToRemove.id );
+
+    // check if quantity is equal to 1, if it is remove that item from the cart
+    if( existingCartItem.quantity === 1 ){
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id );
+    }
+
+    // return back cartitems with matching cart item with reduce quantity
+    return cartItems.map( ( cartItem ) => cartItem.id === cartItemToRemove.id 
+        ? {...cartItem, quantity: cartItem.quantity - 1} 
+        : cartItem
+        );
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
+    removeItemToCart: () => {},
     cartCount: 0
 });
 
@@ -37,8 +54,11 @@ export const CartProvider = ({children}) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     }
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount
-     };
+    const removeItemToCart = ( cartItemToRemove ) =>{
+        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+    }
+
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, removeItemToCart, cartItems, cartCount };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
