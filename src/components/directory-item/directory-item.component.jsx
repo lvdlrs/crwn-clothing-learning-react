7673acdea940dfe866c05f2d197d2c06c8ff2b79
lenmuel <lenmuel.dlrs@gmail.com>
2022-12-11 21:44:24 +0800

    adding category links

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,17 +1,19 @@
+import { Link } from 'react-router-dom';
 import './directory-item.styles.scss';
 
 
 const DirectoryItem = ({ id, imageUrl, title }) => {
     return(
-        <div key={id} className="directory-item-container">
-            <div className="background-image" style={{
-            backgroundImage: `url(${imageUrl})`
-            }}></div>
-            <div className="body">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-            </div>
-      </div>
+            <Link to={"/shop/" + title} key={id} className="directory-item-container">
+                <div className="background-image" style={{
+                backgroundImage: `url(${imageUrl})`
+                }}></div>
+                    <div className="body">
+                    <h2>{title}</h2>
+                    <p>Shop Now</p>
+                </div>
+            </Link>
+
     )
 }
 
